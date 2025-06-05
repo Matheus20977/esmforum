55c3e1bca20fab8bd7d864c762d6e989c9e0659d
Matheus <matheus20977@gmail.com>
2025-06-05 09:16:54 -0300

    Add tests for response registration in modelo.test.js

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -23,3 +23,19 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
+
+test('Testando cadastro de respostas', () => {
+  const id_pergunta = modelo.cadastrar_pergunta('1 + 1 = ?');
+  modelo.cadastrar_resposta(id_pergunta, '2');
+  modelo.cadastrar_resposta(id_pergunta, '3');
+  modelo.cadastrar_resposta(id_pergunta, '4');
+
+  const pergunta = modelo.get_pergunta(id_pergunta);
+  expect(pergunta.texto).toBe('1 + 1 = ?');
+  
+  const respostas = modelo.get_respostas(id_pergunta);
+  expect(respostas.length).toBe(3);
+  expect(respostas[0].texto).toBe('2');
+  expect(respostas[1].texto).toBe('3');
+  expect(respostas[2].texto).toBe('4');
+});
